c434ac5a0817fefa20593af8a8b2b804cea805d5
Michelle Chiza <Chiza.tadiwanashe@gmail.com>
2023-07-24 11:48:15 +0000

    0-printf.c

--- 0-printf.c
+++ 0-printf.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 /**
  * _printf - a custom printf fuction
  * @format: format
@@ -28,21 +30,26 @@ int _printf(const char *format, ...)
 				case 'c':
 				{
 				char c = va_arg(args, int);
+
 				write(1, &c, 1);
 
 				char_count++;
 				break;
 				}
+
 				case 's':
 				{
 				const char *str = va_arg(args, const char *);
+
 				while (*str != '\0')
 				{
 				write(1, str, 1);
 				str++;
 				char_count++;
+				
 				}
 				break;
+				
 				}
 				case '%':
 				write(1, "%", 1);
@@ -55,5 +62,17 @@ int _printf(const char *format, ...)
 		ptr++;
 		}
 		va_end(args);
+
 		return (char_count);
 }
+int main(void)
+{
+	const char *message = "Hello, World!";
+	int num = 42;
+	char character = 'A';
+
+	_printf("Message: %s\nNumber: %d\nCharacter: %c\n", message, num, character);
+
+	return (0);
+}
+
